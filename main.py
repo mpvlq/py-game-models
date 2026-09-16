451446edaa03192b82df85d300e83bfd45efd7de
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-09-17 00:21:17 +0200

    fix flake8 bugs

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ def main() -> None:
         race_data = player_data["race"]
         race, _ = Race.objects.get_or_create(
             name=race_data["name"],
-            defaults={"description":race_data["description"]},
+            defaults={"description": race_data["description"]},
         )
 
         guild_data = player_data["guild"]
         if guild_data is not None:
             guild, _ = Guild.objects.get_or_create(
                 name=(guild_data["name"] if guild_data["name"] else None),
-                defaults={"description": guild_data["description"] if guild_data["description"] else None},
+                defaults={
+                    "description":
+                        guild_data["description"]
+                        if guild_data["description"]
+                        else None
+                },
             )
         else:
             guild = None
